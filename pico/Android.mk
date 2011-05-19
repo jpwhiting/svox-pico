ea2af8319ea840e4feb2e3733bee1ba7c52a7dc7
Narayan Kamath <narayan@google.com>
2011-05-19 14:10:27 +0100

    Minor makefile refactoring.
    This allows folks to depend on libttspico_static and just
    call getTtsEngine() directly instead of all the dlopen()
    dlsym() business.
    
    Change-Id: I7b5b6c94d08b95ade9ba9c995e4bd2a862acdfb6

--- pico/Android.mk
+++ pico/Android.mk
@@ -22,11 +22,14 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
-
-# Build Pico Shared Library
-
+# Build static library containing all PICO code
+# excluding the compatibility code. This is identical
+# to the rule below / except that it builds a shared
+# library.
 LOCAL_PATH:= $(TOP_LOCAL_PATH)/tts
 include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES:= com_svox_picottsengine.cpp svox_ssml_parser.cpp
 
@@ -38,9 +41,28 @@ LOCAL_STATIC_LIBRARIES:= libsvoxpico
 
 LOCAL_SHARED_LIBRARIES:= libcutils libexpat libutils
 
-LOCAL_MODULE:= libttspico
+LOCAL_MODULE:= libttspico_engine
 
 LOCAL_ARM_MODE:= arm
+
+include $(BUILD_STATIC_LIBRARY)
+
+# Build Pico Shared Library. This rule is used by the
+# compatibility code, which opens this shared library
+# using dlsym. This is essentially the same as the rule
+# above, except that it packages things a shared library.
+LOCAL_PATH:= $(TOP_LOCAL_PATH)/tts
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES:= com_svox_picottsengine.cpp svox_ssml_parser.cpp
+LOCAL_C_INCLUDES += \
+	external/svox/pico/lib \
+	external/svox/pico/compat/include
+LOCAL_STATIC_LIBRARIES:= libsvoxpico
+LOCAL_SHARED_LIBRARIES:= libcutils libexpat libutils
+LOCAL_MODULE:= libttspico
 
 include $(BUILD_SHARED_LIBRARY)
 
