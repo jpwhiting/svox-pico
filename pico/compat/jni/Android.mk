e61512ea0d8dc46964cd967fcd0f5ced22ee674b
Joe Onorato <joeo@google.com>
2012-07-11 19:50:59 -0700

    use LOCAL_PATH properly
    Change-Id: I5a177116b976be159042cab51c297861f275daa2

--- pico/compat/jni/Android.mk
+++ pico/compat/jni/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE:= libttscompat
-LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES:= \
 	com_android_tts_compat_SynthProxy.cpp
@@ -17,10 +16,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libmedia \
 	libmedia_native \
 	libutils \
-	libcutils
-
-LOCAL_SHARED_LIBRARIES += libdl
-
-LOCAL_ARM_MODE := arm
+	libcutils \
+	libdl
 
 include $(BUILD_SHARED_LIBRARY)
